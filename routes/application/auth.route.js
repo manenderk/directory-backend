4c027441e27500377b9091de7efd7b1b8c302883
Manender <manender369@gmail.com>
2020-12-06 05:55:11 -0500

    changed import path in auth.route.js

--- routes/application/auth.route.js
+++ routes/application/auth.route.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const passport = require('passport')
-const auth = require('@auth/auth')
+const auth = require('../../auth/auth')
 
 function encodeAuthData (token, user) {
   let data = {
